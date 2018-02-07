44d4242adaf99131d0dc6a86ff61d98efb107890
wenho.hu <marine31504@gmail.com>
2018-02-07 20:31:35 +0800

    add restaurant destroy action for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
 	before_action :authenticate_user!
   before_action :authenticate_admin
-  before_action :set_restaurant, only: [:show, :edit, :update]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 	def index
 		@restaurants = Restaurant.all
 	end
@@ -32,6 +32,12 @@ class Admin::RestaurantsController < ApplicationController
   		flash.now[:alert] = "restaurant was failed to update"
   	end
   end
+  def destroy
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+    flash[:alert] = "restaurant was deleted"
+  end
+
   private
   def restaurant_params
   	params.require(:restaurant).permit(:name, :opening_hours, :tel ,:address, :description)
